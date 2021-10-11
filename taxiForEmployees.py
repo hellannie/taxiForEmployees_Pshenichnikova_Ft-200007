312e6f08521bcf111cdd622fc7c0efc39922747b
hellannie <73584580+hellannie@users.noreply.github.com>
2021-10-11 13:13:26 +0500

    Добавление комментариев и диалога с пользователем

--- taxiForEmployees.py
+++ taxiForEmployees.py
@@ -1,36 +1,44 @@
+# Импорт библиотек для выхода из консоли
 import msvcrt
 import sys
+# Ввод количества сотрудников и проверка на целочисленность
 try:
     employeesN = int(input("Введите количество сотрудников, выражаемое целым числом: "))
 except Exception:
     print("Количество сотрудников не является целым числом, перезапустите программу")
     msvcrt.getch()
     sys.exit()
+# Проверка на вхождение employeesN в диапазон
 if not ((employeesN>=1) and (employeesN<=1000)):
     print("Количество сотрудников выходит за рамки диапазона")
     msvcrt.getch()
     sys.exit()
+# Ввод расстояний до домов сотрудников и проверка на целочисленность
 try:
     employeesKM = list(map(int, input("Введите расстояния, проходимые сотрудниками, в формате строки, содержащие целые числа: ").split()))
 except Exception:
     print("Одно или несколько расстояний не являются целыми числами, перезапустите программу")
     msvcrt.getch()
     sys.exit()
+# Проверка на вхождение каждого элемента employeesKM в диапазон
 for i in employeesKM:
     if (i < 1) and (i > 1000):
         print("Одно или несколько расстояний выходят за рамки диапазона")
         msvcrt.getch()
         sys.exit()
+# Ввод тарифов такси и проверка на целочисленность
 try:
     taxiCost = list(map(int, input("Введите тарифы таксистов в формате строки, содержащие целые числа: ").split()))
 except Exception:
     print("Один или несколько тарифов не являются целыми числами, перезапустите программу")
     msvcrt.getch()
-for i in employeesKM:
+# Проверка на вхождение каждого элемента taxiCost в диапазон
+for i in taxiCost:
     if (i < 1) and (i > 1000):
         print("Один или несколько тарифов выходят за рамки диапазона")
         msvcrt.getch()
         sys.exit()
+# Основная часть работы программы
 taxis = {}
 workers = {}
 for i in range(employeesN):
@@ -38,6 +46,7 @@ for i in range(employeesN):
     workers[employeesKM[i]] = i+1
 employees_sorted = sorted(workers.values())
 taxis_sorted = sorted(taxis.values(),reverse=True)
+# Вывод данных
 print("Выходные данные:")
 print("Номера сотрудников:", end=" ")
 for key in sorted(workers.keys()):
